6888c6571e03761748be6043174393fe5a5301e9
Janell Tanner <janelltanner@Janells-MBP.attlocal.net>
2020-08-26 08:52:46 -0400

    Initialized the repo with app skeleton

---
+++ MyKlemisApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MyKlemisApp.Models;
+
+namespace MyKlemisApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
