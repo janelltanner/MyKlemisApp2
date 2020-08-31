d3fa3a2c774a992fe11514260084c3d2f8743de7
Janell Tanner <janelltanner@Janells-MacBook-Pro.local>
2020-08-31 10:07:33 -0400

    added blank help screen for navigation purposes

---
+++ MyKlemisApp/ViewModels/HelpViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MyKlemisApp.ViewModels
+{
+    public class HelpViewModel: BaseViewModel
+    {
+        public HelpViewModel()
+        {
+            Title = "Help Chat";
+        }
+    }
+}
