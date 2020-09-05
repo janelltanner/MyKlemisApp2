a1ed45a33e76aabc831e8cb76f8ba752868db765
kusyu <kusyu.d@gmail.com>
2020-09-04 20:43:41 -0400

    Adds a LocationPage with a LocationDetailsPage

---
+++ MyKlemisApp/Models/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyKlemisApp.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Hours { get; set; }
+        public string Img { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
--- MyKlemisApp/ViewModels/LocationsViewModel.cs
+++ MyKlemisApp/ViewModels/LocationsViewModel.cs
@@ -1,11 +1,56 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+using MyKlemisApp.Models;
+using MyKlemisApp.Views;
+
 namespace MyKlemisApp.ViewModels
 {
     public class LocationsViewModel : BaseViewModel
     {
+        public ObservableCollection<Location> Locations { get; set; }
+        public Command LoadLocationsCommand { get; set; }
+
         public LocationsViewModel()
         {
             Title = "Locations";
+            Locations = new ObservableCollection<Location>();
+
+            //LoadLocationsCommand = new Command(async () => await ExecuteLoadLocationsCommand());
+
+            //MessagingCenter.Subscribe<NewItemPage, Location>(this, "AddItem", async (obj, item) =>
+            //{
+            //    var newLocation = location as Location;
+            //    Locations.Add(newLocation);
+            //    await DataStore.AddItemAsync(newLocation);
+            //});
         }
+
+        //async Task ExecuteLoadItemsCommand()
+        //{
+        //    IsBusy = true;
+
+        //    try
+        //    {
+        //        Locations.Clear();
+        //        var locations = await DataStore.GetItemsAsync(true);
+        //        foreach (var location in locations)
+        //        {
+        //            Locations.Add(location);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Debug.WriteLine(ex);
+        //    }
+        //    finally
+        //    {
+        //        IsBusy = false;
+        //    }
+        //}
     }
 }
