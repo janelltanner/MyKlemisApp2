f5e82d8e544251389c5b5d5c7e37e8cb9d7e0617
jackle91 <hieu6@gatech.edu>
2020-11-12 13:15:38 -0500

    added calendar

---
+++ MyKlemisApp/ViewModels/CalendarModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+
+namespace MyKlemisApp.ViewModels
+{
+    public class CalendarModel : BaseViewModel
+    {
+        public CalendarModel() { }
+
+        private DateTime? _date;
+        public DateTime? Date
+        {
+            get
+            {
+                return _date;
+            }
+            set
+            {
+                _date = value;
+                OnPropertyChanged(nameof(Date));
+            }
+        }
+
+        private ObservableCollection<XamForms.Controls.SpecialDate> attendances;
+        public ObservableCollection<XamForms.Controls.SpecialDate> Attendances
+        {
+            get
+            {
+                return attendances;
+            }
+            set
+            {
+                attendances = value;
+                OnPropertyChanged(nameof(Attendances));
+            }
+        }
+
+        public Command DateChosen
+        {
+            get
+            {
+                return new Command((obj) =>
+                {
+                    System.Diagnostics.Debug.WriteLine(obj as DateTime?);
+                });
+            }
+        }
+        
+    }
+}
